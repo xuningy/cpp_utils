39b06f49d1353a4b847793d90454984860cd362f
Aditya Dhawale <adityadhawale6@gmail.com>
2019-06-12 16:38:12 -0400

    adds a function to print out eigen matrix in numpy format

--- include/cpp_utils/cpp_utils.h
+++ include/cpp_utils/cpp_utils.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <eigen3/Eigen/Core>
 
 namespace cpp_utils
 {
@@ -15,6 +16,9 @@ std::vector<T> arange(T start, T stop, T step = 1);
 int select_random_element(const std::vector<int> &elements);
 std::vector<std::string> get_files_in_directory(const std::string &directory,
                                                 const std::string &ext);
+template <typename D>
+void print_matrix(const Eigen::MatrixBase<D> &mat, const std::string &func,
+                             const std::string &file, const int line);
 } // namespace cpp_utils
 
 #endif //__CPP_UTILS_H__
--- src/cpp_utils/cpp_utils.cpp
+++ src/cpp_utils/cpp_utils.cpp
@@ -1,6 +1,7 @@
 #include "cpp_utils/cpp_utils.h"
 #include <experimental/filesystem>
 #include <random>
+#define getName(var) #var
 
 std::random_device rd_;
 std::mt19937 gen_(rd_());
@@ -45,6 +46,53 @@ std::string cpp_utils::methodName(const std::string &prettyFunction)
   begin = prettyFunction.substr(0, end).rfind(" ") + 1;
   end -= begin;
   return prettyFunction.substr(begin, end);
+}
+
+template void cpp_utils::print_matrix<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(const Eigen::MatrixBase<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> &mat, const std::string &func, const std::string &file, const int line);
+template void cpp_utils::print_matrix<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(const Eigen::MatrixBase<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> &mat, const std::string &func, const std::string &file, const int line);
+template void cpp_utils::print_matrix<Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(const Eigen::MatrixBase<Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> &mat, const std::string &func, const std::string &file, const int line);
+
+template <typename D>
+void cpp_utils::print_matrix(const Eigen::MatrixBase<D> &mat, const std::string &func,
+                              const std::string &file, const int line)
+{
+  /**
+  * @brief:  Print out a matrix, the function, the file and the line number it's called from
+  * 
+  * This method takes as input an Eigen matrix and prints it out in a numpy format along with 
+  * the debug message of the file, function and line number the print out is called at.
+  * 
+  * @param  mat is the input matrix to be printed
+  * @param func is the C++ macro __PRETTY_FUNCTION__
+  * @param file is the C++ macro __FILE__
+  * @param line is the C++ macro __LINE__
+  * 
+  * @return void
+  */
+  std::string print_message = cpp_utils::methodName(func, file, line);
+  printf("[%s]: Matrix: %s \n", print_message.c_str(), getName(mat));
+  std::cout << "[";
+  for (int i = 0; i < mat.rows()-1; ++i)
+  {
+    if(i != 0)
+      std::cout << " [ ";
+    else 
+      std::cout << "[ ";
+    for (int j = 0; j < mat.cols() - 1; ++j)
+    {
+      std::cout << mat(i, j) << ", ";
+    }
+    std::cout << mat(i, mat.cols() - 1);
+    std::cout << "]\n";
+  }
+  std::cout << " [ ";
+  for (int j = 0; j < mat.cols() - 1; ++j)
+  {
+    std::cout << mat(mat.rows() - 1, j) << ", ";
+  }
+  std::cout << mat(mat.rows() - 1, mat.cols() - 1);
+  std::cout << "]";
+  std::cout << "]\n\n";
 }
 
 template std::vector<int>
